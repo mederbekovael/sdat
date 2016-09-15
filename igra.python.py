dc5416134153986129daa620e8eb34e6e17b3eb9
unknown <mederbekova_e@12-04.iuca.loc>
2016-09-15 17:28:45 +0600

    fff

---
+++ igra.python.py
@@ -0,0 +1,198 @@
+from random import randint
+chislo = randint(1,6)
+print "komputer zagodal chislo ot 1 do 6"
+abs (n)= int("vvedite chislo ot 1 do 6")
+summa =  randint(1,6)
+if  chislo > 6:
+	print "pokajite chislo ot 1 do 6"
+elif chislo < 0:
+	print "pokajite chislo ot 1 do 6"
+elif summa == chislo1:
+	n = summa - chislo1
+	itog = 6
+	print "vashe itog: "
+	print itog
+elif summa > chislo1:
+	n = summa (chislo1)
+    abs n == 1:
+    	itog = itog + 5
+    	print "vash itog: "
+    	print itog
+    elif n == 2:
+    	itog = itog + 4
+    	print "vash itog: "
+    	print itog
+    elif n == 3:
+    	itog = itog + 3
+    	print "vash itog: "
+    	print itog
+     elif n == 4:
+    	itog = itog + 2
+    	print "vash itog: "
+    	print itog	
+     elif n == 5:
+    	itog = itog + 1
+    	print "vash itog: "
+    	print itog	
+     elif n == 6:
+    	itog = itog + 0
+    	print "vash itog: "
+    	print itog
+abs = int("vvedite chislo ot 1 do 6")
+chislo2 = int("vvedite chislo ot 1 do 6")
+summa =  randint(1,6)
+if  chislo > 6:
+    print "pokajite chislo ot 1 do 6"
+elif chislo < 0:
+    print "pokajite chislo ot 1 do 6"
+elif summa == chislo2:
+    n = summa - chislo2
+    itog = 6
+    print "vashe itog: "
+    print itog
+elif summa > chislo2:
+    n = summa (chislo2)
+    abs(n):
+    if n == 1:
+        itog = itog + 5
+        print "vash itog: "
+        print itog
+    elif n == 2:
+        itog = itog + 4
+        print "vash itog: "
+        print itog
+    elif n == 3:
+        itog = itog + 3
+        print "vash itog: "
+        print itog
+     elif n == 4:
+        itog = itog + 2
+        print "vash itog: "
+        print itog  
+     elif n == 5:
+        itog = itog + 1
+        print "vash itog: "
+        print itog  
+     elif n == 6:
+        itog = itog + 0
+        print "vash itog: "
+        print itog
+
+chislo3 = int("vvedite chislo ot 1 do 6")
+summa =  randint(1,6)
+if  chislo > 6:
+    print "pokajite chislo ot 1 do 6"
+elif chislo < 0:
+    print "pokajite chislo ot 1 do 6"
+elif summa == chislo3:
+    n = summa - chislo3
+    itog = 6
+    print "vashe itog: "
+    print itog
+elif summa > chislo3:
+    n = summa (chislo3)
+    abs(n):
+    if n == 1:
+        itog = itog + 5
+        print "vash itog: "
+        print itog
+    elif n == 2:
+        itog = itog + 4
+        print "vash itog: "
+        print itog
+    elif n == 3:
+        itog = itog + 3
+        print "vash itog: "
+        print itog
+     elif n == 4:
+        itog = itog + 2
+        print "vash itog: "
+        print itog  
+     elif n == 5:
+        itog = itog + 1
+        print "vash itog: "
+        print itog  
+     elif n == 6:
+        itog = itog + 0
+        print "vash itog: "
+        print itog
+
+chislo4 = int("vvedite chislo ot 1 do 6")
+summa =  randint(1,6)
+if  chislo > 6:
+    print "pokajite chislo ot 1 do 6"
+elif chislo < 0:
+    print "pokajite chislo ot 1 do 6"
+elif summa == chislo4:
+    n = summa - chislo4
+    itog = 6
+    print "vashe itog: "
+    print itog
+elif summa > chislo4:
+    n = summa (chislo4)
+    abs(n):
+    if n == 1:
+        itog = itog + 5
+        print "vash itog: "
+        print itog
+    elif n == 2:
+        itog = itog + 4
+        print "vash itog: "
+        print itog
+    elif n == 3:
+        itog = itog + 3
+        print "vash itog: "
+        print itog
+     elif n == 4:
+        itog = itog + 2
+        print "vash itog: "
+        print itog  
+     elif n == 5:
+        itog = itog + 1
+        print "vash itog: "
+        print itog  
+     elif n == 6:
+        itog = itog + 0
+        print "vash itog: "
+        print itog
+        
+
+chislo5 = int("vvedite chislo ot 1 do 6")
+summa =  randint(1,6)
+if  chislo > 6:
+    print "pokajite chislo ot 1 do 6"
+elif chislo < 0:
+    print "pokajite chislo ot 1 do 6"
+elif summa == chislo5:
+    n = summa - chislo5
+    itog = 6
+    print "vashe itog: "
+    print itog
+elif summa > chislo5:
+    n = summa (chislo2)
+    abs(n):
+    if n == 1:
+        itog = itog + 5
+        print "vash itog: "
+        print itog
+    elif n == 2:
+        itog = itog + 4
+        print "vash itog: "
+        print itog
+    elif n == 3:
+        itog = itog + 3
+        print "vash itog: "
+        print itog
+     elif n == 4:
+        itog = itog + 2
+        print "vash itog: "
+        print itog  
+     elif n == 5:
+        itog = itog + 1
+        print "vash itog: "
+        print itog  
+     elif n == 6:
+        itog = itog + 0
+        print "vash itog: "
+        print itog
+
---
+++ ttt.py
@@ -0,0 +1,31 @@
+from random import randint
+import random
+abs player_name():
+   name1 = input("komputer:")
+   name2 = input("vy:")
+   return name1, name2
+   if chislo == 1:
+    print ("vypoli costi naminalom v odin ")
+   if chislo == 2:
+    print("vypoli costi naminalom v dva")
+   if chislo == 3:
+    print(" vypoli costi naminalom v tri")
+   if chislo == 4:
+    print("vypoli costi naminalom v chetyre")
+   if chislo == 5:
+    print("vypoli costi naminalom v pyat")
+   if chislo == 6:
+    print("vypoli costi naminalom v shest")
+
+   if roll_dice2 == 1:
+    print("i odin")
+   if roll_dice2 == 2:
+    print("i dva")
+   if roll_dice2 == 3:
+    print("i tri")
+   if roll_dice2 == 4:
+    print("i chetyre")
+   if roll_dice2 == 5:
+    print("i pyat")
+   if roll_dice2 == 6:
+    print("i shest")
